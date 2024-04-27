f7b95db7808bae35072a1b6696b451a8c3187de6
Dipanker Sharma <dipankar.kr2005@gmail.com>
2024-04-27 09:58:24 +0530

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className="w-full h-screen flex">
+      <nav className='h-full w-[15%] bg-zinc-100 flex flex-col items-center pt-5'>
+        <a className='py-3 px-5 border rounded border-blue-300 text-blue-400 ' href="/create">Add new product</a>
+        <hr className='w-[80%] my-3' />
+        <h1 className='text-2xl mb-3 w-[80%]'>Category Filter</h1>
+        <ul  className='  w-[80%]'>
+          <li className='mb-3 flex items-center gap-2'>
+             <span className='w-[15px] h-[15px]  bg-blue-100 rounded-full'></span>Category-1</li>
+          <li className='mb-3 flex items-center gap-2'>
+             <span className='w-[15px] h-[15px]  bg-red-100 rounded-full'></span>Category-2</li>
+          <li className='mb-3 flex items-center gap-2'>
+             <span className='w-[15px] h-[15px]  bg-green-100 rounded-full'></span>Category-1</li>
+        </ul>
+      </nav>
+      <div className="home w-[85%] h-full bg-red-100"></div>
+    </div>
+  )
+}
+
+export default App
